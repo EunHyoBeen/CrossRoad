5f6f75fb494baabdd323f370af2892d105d4e7dd
hyobeen12 <aszx8683@naver.com>
2024-11-15 19:57:38 +0900

    add scripts

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,40 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public interface IHit
+{
+    public void GetHit();
+}
+
 public class PlayerController : MonoBehaviour
 {
-    public int moveRange = 10;
-    private Vector2 curMoveInput;
+    public float moveDistance = 1;
+    private Vector3 curPos;
+    private Vector3 moveValue;
+    public float moveTime = 0.4f;
+    public float colliderDistCheck = 1.1f;
 
-    private Rigidbody rb;
+    public ParticleSystem particle = null;
+    public Transform chick = null;
+    public bool isDead = false;
 
-    private void Awake()
+    void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        moveValue = Vector3.zero;
+        curPos = transform.position;
     }
 
-    private void FixedUpdate()
+    public void Move(InputAction.CallbackContext context)
     {
-        Move();
-    }
+        Vector3 input = context.ReadValue<Vector3>();
 
-    public void OnMoveInput(InputAction.CallbackContext context)
-    {
-        if(context.phase == InputActionPhase.Started)
+        if (input.magnitude > 1f) return;
+
+        if (context.performed)
         {
-            curMoveInput = context.ReadValue<Vector2>();
-        }
-        if(context.phase == InputActionPhase.Canceled)
-        {
-            curMoveInput = Vector2.zero;
+            if (input.magnitude == 0f)
+            {
+                Moving(transform.position + moveValue);
+                Rotate(moveValue);
+                moveValue = Vector3.zero;
+            }
+            else
+            {
+                moveValue = input * moveDistance;
+            }
         }
     }
 
-    private void Move()
+    void Moving(Vector3 pos)
     {
-        Vector3 dir = transform.forward * curMoveInput.y + transform.right * curMoveInput.x;
-        dir *= moveRange;
-        dir.y = rb.velocity.y;
-        rb.velocity = dir;
+        LeanTween.move(this.gameObject, pos, moveTime).setOnComplete(() => { if (pos.z > curPos.z) SetMoveForwardState(); });
+    }
+
+    void Rotate(Vector3 pos)
+    {
+        chick.rotation = Quaternion.Euler(270, pos.x * 90, 0);
+    }
+
+    void SetMoveForwardState()
+    {
+        Manager.instance.UpdateDistanceCount();
+        curPos = transform.position;
+    }
+
+    public void GetHit()
+    {
+        Manager.instance.GameOver();
+        isDead = true;
+        ParticleSystem.EmissionModule em = particle.emission;
+        em.enabled = true;
     }
 }
